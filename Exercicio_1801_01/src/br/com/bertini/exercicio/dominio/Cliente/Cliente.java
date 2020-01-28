a4f1b1cf6c9bbbfd46a0cb00c7572ec298778eb8
ribeirohenrique <henrique1258@gmail.com>
2020-01-28 13:25:59 -0300

    Segundo commit

--- Exercicio_1801_01/src/br/com/bertini/exercicio/dominio/Cliente/Cliente.java
+++ Exercicio_1801_01/src/br/com/bertini/exercicio/dominio/Cliente/Cliente.java
@@ -1,30 +1,30 @@
 package br.com.bertini.exercicio.dominio.Cliente;
 
-public class Cliente {
+public class Cliente { //É criado uma classe denominada Cliente onde esta irá conter Nome, Idade e Profissão.
 	
-	private String nome;
-	private Integer idade;
-	private String profissao;
+	private String nome; //Variavel String, aceita somente dados em ""
+	private Integer idade; //Variavel idade como Integer aceita somente valores inteiros
+	private String profissao; //Variavel String aceita somente daddos em "" 
 	
 	//Depois de definido os valores a serem obtidos, utilizar a ferramenta Source > Generate Getters and Setters
-	public String getNome() {
-		return nome;
+	public String getNome() { //"Puxa" a variavel nome com o valor obtido no executor
+		return nome; //retorna o dado armazenado para a tela
 	}
-	public void setNome(String nome) {
-		this.nome = nome;
+	public void setNome(String nome) { //"Define" a variavel nome com o valor obtido no executor
+		this.nome = nome; 
 	}
 	
-	public Integer getIdade() {
-		return idade;
+	public Integer getIdade() { //"Puxa" a variavel idade com o valor obtido no executor
+		return idade; //retorna o dado armazenado para a tela
 	}
-	public void setIdade(Integer idade) {
+	public void setIdade(Integer idade) { //"Define" a variavel nome com o valor obtido no executor
 		this.idade = idade;
 	}
 	
-	public String getProfissao() {
-		return profissao;
+	public String getProfissao() { //"Puxa" a variavel profissao com o valor obtido no executor
+		return profissao; //retorna o dado armazenado para a tela
 	}
-	public void setProfissao(String profissao) {
+	public void setProfissao(String profissao) { //"Define" a variavel profissao com o valor obtido no executor
 		this.profissao = profissao;
 	}
 	
